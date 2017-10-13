bfbca1ff572e995c59a4e0641d08f97d89ec7368
Nehemias Ulloa <nulloa1@iastate.edu>
2017-10-13 18:50:19 -0500

    Update to current working code

--- HW/HW3/HW3.R
+++ HW/HW3/HW3.R
@@ -1,33 +1,34 @@
-library(dlm)
-
-lake <- read.table("lakeSuperior.dat", col.names = c("year","precipitation_inches"))
+library("dlm")
+library("dplyr")
+library("reshape2")
+library("ggplot2")
+OGlake <- read.table("lakeSuperior.dat", col.names = c("year","precipitation_inches"))
+lake <- OGlake$precipitation_inches
 plot(lake, type ='o', col = "seagreen")
 
-lake <- lake$precipitation_inches
 
-buildFun <- function(x){ dlmModPoly(2, dV = exp(x[1]), dW=c(0,exp(x[2])), m0=c(10,10), C0=matrix(c(10,0,0,5), 2, 2)) }
+# using mle as starting values
+buildFun <- function(x){ dlmModPoly(2, dV = exp(x[1]), dW=c(0,exp(x[2])), m0=c(30,9), C0=matrix(c(100^2,0,0,1^2), 2, 2)) }
 fit <- dlmMLE(lake, parm = c(1,1), build = buildFun)
-(dlmLake <- buildFun(fit$par))
-StructTS(x = lake, type = "trend")
-FiltD   <- dlm::dlmFilter(lake, dlmLake)
-bsample <- dlm::dlmBSample(FiltD)
-
-lakeFilt <- dlmFilter(lake, dlmLake)
-bsample  <- dlmBSample(lakeFilt)
+sval <- list(v=exp(fit$par[1]), w=exp(fit$par[2]))
 
 
 mcmc1 <- function(y, niter, sval){
-  
   # Summary Stats
-  n <- nrow(y)
+  n <- length(y)
   
   # setup storage
   thetakeep <- matrix(data=NA, ncol=2*(n+1), nrow=niter)
   phikeep   <- matrix(data=NA, ncol=2, nrow=niter)
   
+  # setup initialization
+  sigma  <- sval$v
+  sigmab <- sval$w
+  
   for(i in 1:niter){
+    if(i%%(niter/10) == 0){print(paste(100*i/niter,"% of iterations complete", sep=""))}
     # sample theta jointly via backwards sampling
-    dlmM    <- dlm::dlmModPoly(2, dV = sval$v, dW=sval$w)
+    dlmM    <- dlm::dlmModPoly(2, dV = sigma, dW=c(0,sval$w))
     FiltD   <- dlm::dlmFilter(y, dlmM)
     bsample <- dlm::dlmBSample(FiltD)
     
@@ -44,9 +45,6 @@ mcmc1 <- function(y, niter, sval){
     phikeep[i,2]   <- sigmab
   }
   
-  #colnames(thetakeep)[1:n+1] <- paste("mu[",seq(0:n),"]",sep="")
-  #colnames(thetakeep)[-c(1:n+1)] <- paste("beta[",seq(0:n),"]",sep="")
-  #colnames(thetakeep)[-c(1:n+1)] <- paste("beta[",seq(0:n),"]",sep="")
   # end of sampling mechanism
   return(list("theta"=thetakeep, "phi"=phikeep))
 }
@@ -68,17 +66,19 @@ mcmc2 <- function(y, niter, sval, Sigma){
   
   for(i in 1:niter){
     if(i%%(niter/10) == 0){print(paste(100*i/niter,"% of iterations complete", sep=""))}
+    
     # sample phi 
     dphi <- function(phi, y, a, b){
       dinvgamma = function(x, a, b) dgamma(1/x,a,b)/x^2
       tmpPhi <- NULL
+      dlmM      <- dlm::dlmModPoly(2, dV = phi[1], dW=c(0,phi[2]), m0=c(10,10), C0=matrix(c(10,0,0,5), 2, 2))
+      FiltD     <- dlm::dlmFilter(y, dlmM)
+      Rt        <- dlm::dlmSvd2var(FiltD$U.R, FiltD$D.R)
       for(t in 1:length(y)){
-        dlmM      <- dlm::dlmModPoly(2, dV = phi[1], dW=c(0,phi[2]))
-        FiltD     <- dlm::dlmFilter(y[t], dlmM)
-        Rt        <- dlm::dlmSvd2var(FiltD$U.R, FiltD$D.R)
-        tmpPhi[t] <- dnorm(y[t], FiltD$a[1], Rt[[1]][1,1])
+        prop_var <- dlmM$FF %*% Rt[[t]] %*% t(dlmM$FF) + dlmM$V
+        tmpPhi[t] <- dnorm(y[t], FiltD$f[t], sqrt(prop_var), log=TRUE)
       }
-      return(prod(tmpPhi)*dinvgamma(phi[1], a, b)*dinvgamma(phi[2], a, b))
+      return(sum(tmpPhi) + log(dinvgamma(phi[1], a, b)) + log(dinvgamma(phi[2], a, b)))
     }
     
     propphi <- MASS::mvrnorm(n = 1, prevphi, Sigma)
@@ -87,7 +87,8 @@ mcmc2 <- function(y, niter, sval, Sigma){
       phi <- prevphi
     }else{
       u <- runif(1, 0, 1)
-      ifelse(u > dphi(prevphi, y, 1, 1)/dphi(propphi, y, 1, 1), phi <- prevphi, phi <- propphi)
+      # print(dphi(prevphi, y, 1, 1)/dphi(propphi, y, 1, 1))
+      ifelse(u > exp(dphi(propphi, y, 1, 1)) / exp(dphi(prevphi, y, 1, 1)), phi <- prevphi, phi <- propphi)
     }
     
     # sample theta
@@ -106,16 +107,7 @@ mcmc2 <- function(y, niter, sval, Sigma){
   return(list("theta"=thetakeep, "phi"=phikeep))
 }
 
-# using mles found above as starting values
-Sigma <- matrix(data=c(10,0,0,0.05), 2, 2, byrow=TRUE)
-system.time(res2 <- mcmc2(lake, 1000, sval, Sigma))
-
-
-
-
-
-
-ggplot() + geom_histogram(aes(x = rnorm(100, 0.5, 0.0005), y = ..density../1000),bin=100)
-ggplot() + geom_histogram(aes(x = rnorm(100, 0.5, 0.0005), y = ..density..),bin=100)
-
+Sigma0 <- matrix(data=c(10,0,0,0.05), 2, 2, byrow=TRUE)
+sigres <- mcmc2(lake, 1000, sval, Sigma0)
+covmat <- cov(sigres$phi)
 
